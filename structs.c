c8d1bf688dd94769a82fb9552d4ff6296297589c
Thomas Lucking <telucking@gmail.com>
2026-02-28 15:59:34 +0100

    feat modified one exercise

--- structs.c
+++ structs.c
@@ -7,14 +7,18 @@ struct Node{
     struct Node *next; // pointer for the next node.
 };
 
-
-
 int main()
 {
     struct Node *node1 = malloc(sizeof(struct Node)); 
-    node1->value = 15;                                
-    printf("%d", node1->value);                       
-    free(node1);                                      
+    struct Node *node2 = malloc(sizeof(struct Node));
+
+    node1->value = 15;
+    node2->value = 20;
+    node1->next = node2;
+
+    printf("%d", node2->next->value);                       
+    free(node1); 
+    free(node2);                                     
 
 
     return 0;
